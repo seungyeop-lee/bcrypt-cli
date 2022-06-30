741eb69f677f4699b6835b9ba4988172242aa6f1
seungyeop-lee <xbeast7@gmail.com>
2022-06-30 12:41:03 +0900

    기존 output은 verbose 모드로 옮기고, 짧은 결과만 return하도록 변경

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,32 +4,40 @@ import (
 	"fmt"
 	"os"
 
+	logger "github.com/seungyeop-lee/bcrypt-cli/log"
+
 	"github.com/seungyeop-lee/bcrypt-cli/app"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "bcrypt-cli",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		verbose, _ := cmd.Flags().GetBool("verbose")
+		logger.IsVerbose = verbose
+	},
 }
 
 var generateCmd = &cobra.Command{
 	Use:     "generate",
-	Short:   "generate hash",
+	Short:   "Generate hash",
 	Example: "bcrypt-cli generate -p myPassword",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		password, _ := cmd.Flags().GetString("password")
-		fmt.Printf("input password: %s\n", password)
+		logger.Info(fmt.Sprintf("input password: %s", password))
 
 		cost, _ := cmd.Flags().GetInt("cost")
 		generator := app.NewGenerator(cost)
-		fmt.Printf("input cost: %d\n", generator.Cost())
+		logger.Info(fmt.Sprintf("input cost: %d", generator.Cost()))
 
 		generatedHash, err := generator.Generate(password)
 		if err != nil {
 			return err
 		}
 
-		fmt.Printf("generated hash: %s\n", generatedHash)
+		logger.Info(fmt.Sprintf("generated hash: %s", generatedHash))
+
+		fmt.Print(generatedHash)
 
 		return nil
 	},
@@ -37,11 +45,11 @@ var generateCmd = &cobra.Command{
 
 var costCmd = &cobra.Command{
 	Use:     "cost",
-	Short:   "calculate cost from hash",
-	Example: "bcrypt-cli cost -i '$2a$10$yQiVOOeFcuwYRSxjmXxb1.l5EDXn66Mqg0w7Hr/W6vbV1SYcAnwt6'",
+	Short:   "Calculate cost from hash",
+	Example: "bcrypt-cli cost -i '$2a$10$iJ/CnWkU8efsEKnnR14vl.MYVfy9adcAXxpPeiLrGaHTaKx5JBbse'",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		hash, _ := cmd.Flags().GetString("hash")
-		fmt.Printf("input hash: %s\n", hash)
+		logger.Info(fmt.Sprintf("input hash: %s", hash))
 
 		checker := app.NewChecker()
 		cost, err := checker.Cost(hash)
@@ -49,7 +57,9 @@ var costCmd = &cobra.Command{
 			return err
 		}
 
-		fmt.Printf("cost: %d\n", cost)
+		logger.Info(fmt.Sprintf("cost: %d", cost))
+
+		fmt.Print(cost)
 
 		return nil
 	},
@@ -57,27 +67,32 @@ var costCmd = &cobra.Command{
 
 var checkCmd = &cobra.Command{
 	Use:     "check",
-	Short:   "check valid password and hash",
-	Example: "bcrypt-cli check -p qwer -i '$2a$10$yQiVOOeFcuwYRSxjmXxb1.l5EDXn66Mqg0w7Hr/W6vbV1SYcAnwt6'",
+	Short:   "Check valid password and hash",
+	Long:    "As a result of checking the password and hash, 0 if valid and 1 if invalid are returned as the status code",
+	Example: "bcrypt-cli check -p myPassword -i '$2a$10$iJ/CnWkU8efsEKnnR14vl.MYVfy9adcAXxpPeiLrGaHTaKx5JBbse'",
 	Run: func(cmd *cobra.Command, args []string) {
 		password, _ := cmd.Flags().GetString("password")
-		fmt.Printf("input password: %s\n", password)
+		logger.Info(fmt.Sprintf("input password: %s", password))
 
 		hash, _ := cmd.Flags().GetString("hash")
-		fmt.Printf("input hash: %s\n", hash)
+		logger.Info(fmt.Sprintf("input hash: %s", hash))
 
 		checker := app.NewChecker()
 		err := checker.Check(password, hash)
 
 		if err == nil {
-			fmt.Printf("check result: %s\n", "success")
+			logger.Info(fmt.Sprintf("check result: %s", "success"))
 		} else {
-			fmt.Printf("check result: %s\n", "failure")
+			logger.Info(fmt.Sprintf("check result: %s", "failure"))
+			_, _ = fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
 	},
 }
 
 func Execute() {
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "run by verbose mode (for debug)")
+
 	generateCmd.Flags().StringP("password", "p", "", "input password")
 	generateCmd.Flags().IntP("cost", "c", 10, "input cost for hashing")
 	_ = generateCmd.MarkFlagRequired("password")
